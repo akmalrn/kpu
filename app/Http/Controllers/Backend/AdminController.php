0ccac5850a5646a448f54b9da3ccef8b7887e229
akmalrn <legendsmystic60@gmail.com>
2025-04-25 16:04:16 +0700

    Kpu

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Indikator;
+use App\Models\Backend\KategoriIndikator;
+use App\Models\Backend\Prestasi;
+use App\Models\Backend\Siswa;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('backend.index');
+        $SiswaTotal = Siswa::count();
+        $KategoriTotal = KategoriIndikator::count();
+        $IndikatorTotal = Indikator::count();
+        $PrestasiTotal = Prestasi::count();
+        return view('backend.index', compact('SiswaTotal', 'KategoriTotal', 'IndikatorTotal', 'PrestasiTotal'));
     }
 }
