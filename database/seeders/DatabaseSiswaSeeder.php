052bb9fbf06b7d9d386dd896e5863f54c16aec59
akmalrn <legendsmystic60@gmail.com>
2025-05-01 11:42:56 +0700

    kpu seeder

---
+++ database/seeders/DatabaseSiswaSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Backend\Siswa;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Siswa::Create([
+            'nis' => '12200765',
+            'nama' => 'Siswa 1',
+            'kelas' => 'XII PPLG',
+            'tipe' => 'reguler',
+            'password' => Hash::make('123')
+        ]);
+
+        Siswa::Create([
+            'nis' => '12200766',
+            'nama' => 'Siswa 1',
+            'kelas' => 'XII PPLG',
+            'tipe' => 'reguler',
+            'password' => Hash::make('123')
+        ]);
+
+        Siswa::Create([
+            'nis' => '12200767',
+            'nama' => 'Siswa 1',
+            'kelas' => 'XII PPLG',
+            'tipe' => 'reguler',
+            'password' => Hash::make('123')
+        ]);
+    }
+}
